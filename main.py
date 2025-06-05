ea8b714fdff1f98aaaabe839e19cfa5220138121
Yupara <parviz.yusupov03@mail.ru>
2025-06-06 04:35:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ app = FastAPI()
 
 @app.post("/register", response_model=UserOut)
 def register(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.username == user.username).first()
-    if db_user:
+    existing_user = db.query(User).filter(User.username == user.username).first()
+    if existing_user:
         raise HTTPException(status_code=400, detail="Пользователь уже существует")
-    
-    new_user = User(username=user.username, password=user.password)  # в реальности — хешируй пароль!
+
+    new_user = User(username=user.username, password=user.password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
