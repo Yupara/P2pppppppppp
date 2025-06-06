43756f2fc5fda73e09afe01be657a46a446b682a
Yupara <parviz.yusupov03@mail.ru>
2025-06-07 04:30:30 +0500

    Create styles.py

---
+++ styles.py
@@ -0,0 +1,17 @@
+body {
+    background-color: #003300;
+    color: #ffffff;
+    font-family: Arial, sans-serif;
+}
+
+button {
+    background-color: #006600;
+    color: #ffffff;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #004400;
+}
