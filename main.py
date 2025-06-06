88e834d1931d506ca52b1d600afb423a176b8c9d
Yupara <parviz.yusupov03@mail.ru>
2025-06-07 04:27:42 +0500

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import sys
-print(sys.path)  # Посмотрите, есть ли /app в путях
-import os
-print(os.listdir("/app"))  # Посмотрите, есть ли database в /app
-print(os.listdir("/app/database"))  # Посмотрите содержимое database
