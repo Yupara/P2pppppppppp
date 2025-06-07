351aa1bf188bfd20ab67f71b5546204a007bb311
Yupara <parviz.yusupov03@mail.ru>
2025-06-07 09:55:03 +0500

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Dashboard from "./components/dashboard";
-import Login from "./components/login";
-import AdminPanel from "./components/adminpanel";
+import Login from "./Login";
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/admin" component={AdminPanel} />
-            </Switch>
-        </Router>
+        <div>
+            <Login />
+        </div>
     );
 }
 
