51a4a1a5910e06ca8407beeeb2965b8085762bf3
Yupara <parviz.yusupov03@mail.ru>
2025-06-10 04:39:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI
+app = FastAPI()
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
