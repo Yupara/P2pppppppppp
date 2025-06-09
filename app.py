8ef7206ded1629e4f27811274f6feff0292267a3
Yupara <parviz.yusupov03@mail.ru>
2025-06-10 04:35:43 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+fake_users = {}
+ads = []
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/register")
+def register_form(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.post("/register")
+def register(username: str = Form(...), password: str = Form(...)):
+    fake_users[username] = password
+    return RedirectResponse("/login", status_code=302)
+
+@app.get("/login")
+def login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+    if fake_users.get(username) == password:
+        return RedirectResponse("/market", status_code=302)
+    return RedirectResponse("/login", status_code=302)
+
+@app.get("/market")
+def market(request: Request):
+    return templates.TemplateResponse("market.html", {"request": request, "ads": ads})
