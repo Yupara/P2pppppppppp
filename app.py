99e95770f72dddd0492d29e66d9b9e5f81102eab
Yupara <parviz.yusupov03@mail.ru>
2025-06-10 04:44:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,15 @@
-from fastapi import FastAPI
-app = FastAPI()
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Подключаем папки
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-fake_users = {}
-ads = []
-
-@app.get("/")
-def index(request: Request):
+# Главная страница
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-@app.get("/register")
-def register_form(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-@app.post("/register")
-def register(username: str = Form(...), password: str = Form(...)):
-    fake_users[username] = password
-    return RedirectResponse("/login", status_code=302)
-
-@app.get("/login")
-def login_form(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-@app.post("/login")
-def login(username: str = Form(...), password: str = Form(...)):
-    if fake_users.get(username) == password:
-        return RedirectResponse("/market", status_code=302)
-    return RedirectResponse("/login", status_code=302)
-
-@app.get("/market")
-def market(request: Request):
-    return templates.TemplateResponse("market.html", {"request": request, "ads": ads})
