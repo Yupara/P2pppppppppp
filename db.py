bb700f2a40b1ce50d08f22f462e8a43ee886711d
Yupara <parviz.yusupov03@mail.ru>
2025-06-13 12:22:08 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,15 +1,11 @@
-# db.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL вашей БД
 DATABASE_URL = "sqlite:///./p2p.db"
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -18,7 +14,6 @@ SessionLocal = sessionmaker(
 )
 Base = declarative_base()
 
-# Зависимость для FastAPI
 def get_db():
     db = SessionLocal()
     try:
