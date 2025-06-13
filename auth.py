dddcdcaac86d0a78a4656167f99f5894ad149f40
Yupara <parviz.yusupov03@mail.ru>
2025-06-13 12:30:37 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,28 +1,20 @@
-from fastapi import (
-    APIRouter, Request, Depends,
-    Form, HTTPException, status
-)
+# auth.py
+
+from fastapi import APIRouter, Request, Depends, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 
 import crud
-from db import get_db  # <-- импорт из db.py
+from db import get_db
 
 router = APIRouter()
 
-@router.get("/login", response_class=HTMLResponse)
-def login_form(request: Request):
-    return HTMLResponse("<h1>Login Form</h1>")
+@router.get("/register", response_class=HTMLResponse)
+def register_form(request: Request):
+    return HTMLResponse("<h1>Регистрация</h1>")
 
-@router.post("/login")
-def login(
-    request: Request,
-    username: str = Form(...),
-    password: str = Form(...),
-    db: Session = Depends(get_db)
-):
-    user = crud.get_user(db, username)
-    if not user or user.password != password:
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Неверные данные")
-    request.session["user_id"] = user.id
+@router.post("/register")
+def register(request: Request, username: str = Form(...), password: str = Form(...),
+             db: Session = Depends(get_db)):
+    crud.create_user(db, username, password)
     return RedirectResponse("/market", status_code=302)
