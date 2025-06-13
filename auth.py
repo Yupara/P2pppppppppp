25f428078142458108f1e8a349b3828c2adba405
Yupara <parviz.yusupov03@mail.ru>
2025-06-13 13:09:35 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,20 +1,42 @@
 # auth.py
 
-from fastapi import APIRouter, Request, Depends, Form
+from fastapi import (
+    APIRouter, Request, Depends,
+    Form, HTTPException, status
+)
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 import crud
+import models
 from db import get_db
 
+templates = Jinja2Templates(directory="templates")
 router = APIRouter()
 
-@router.get("/register", response_class=HTMLResponse)
-def register_form(request: Request):
-    return HTMLResponse("<h1>Регистрация</h1>")
+@router.get("/login", response_class=HTMLResponse)
+def login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request, "error": None})
 
-@router.post("/register")
-def register(request: Request, username: str = Form(...), password: str = Form(...),
-             db: Session = Depends(get_db)):
-    crud.create_user(db, username, password)
+@router.post("/login")
+def login(
+    request: Request,
+    username: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    user = db.query(models.User)\
+             .filter_by(username=username, password=password)\
+             .first()
+    if not user:
+        return templates.TemplateResponse("login.html", {
+            "request": request, "error": "Неверные данные"
+        })
+    if user.is_blocked:
+        return templates.TemplateResponse("login.html", {
+            "request": request,
+            "error": "Ваш аккаунт заблокирован из-за 10 отмен"
+        })
+    request.session["user_id"] = user.id
     return RedirectResponse("/market", status_code=302)
