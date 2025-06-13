1aa3cd144cc8901ee22570c3f857b7a9ed351662
Yupara <parviz.yusupov03@mail.ru>
2025-06-13 11:56:36 +0500

    Create notifications.py

---
+++ notifications.py
@@ -0,0 +1,10 @@
+# сюда складываем функции отправки email/SMS
+def send_sms(to: str, text: str):
+    # TODO: интеграция с провайдером
+    pass
+
+def send_email(to: str, subject: str, html: str):
+    # TODO: интеграция с SMTP
+    pass
+
+# фоновый таск ежедневной статистики, крупные сделки, споры
