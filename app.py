107c9db5bcaf58f687b7f279133cd49e42b3e501
Yupara <parviz.yusupov03@mail.ru>
2025-06-13 12:21:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-# app.py
-
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
-from db import engine, Base          # <-- из db.py
-import models                        # <-- создаёт таблицы через Base.metadata
-from auth import router as auth_router
-from payment import router as pay_router
-# ... другие роутеры ...
+from db import engine, Base  # <-- Base и engine из db.py
+import models                # <-- здесь регистрируются все модели
 
-# Создать все таблицы
+from auth import router as auth_router
+from payment import router as payment_router
+
+# создаём таблицы
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="CHANGE_THIS_SECRET")
+app.add_middleware(SessionMiddleware, secret_key="REPLACE_WITH_SECRET")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Подключаем маршруты
 app.include_router(auth_router, prefix="", tags=["auth"])
-app.include_router(pay_router, prefix="", tags=["payment"])
-# ... остальные роутеры ...
+app.include_router(payment_router, prefix="", tags=["payment"])
